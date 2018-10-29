5879fe59cce6043bbf7ae8b1e08f3b0bb06ca467
acostapazo <costapazo@gmail.com>
2018-10-29 20:47:17 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -26,6 +26,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
     'matplotlib.sphinxext.plot_directive'
     ]
 
